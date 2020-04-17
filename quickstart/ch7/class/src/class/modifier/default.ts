b2bca093aeec64fc8b83104701385dc0a753f626
kooltz <hgshin83@gmail.com>
2020-04-18 01:53:35 +0900

    ch7 - 클래스와 인터페이스

---
+++ quickstart/ch7/class/src/class/modifier/default.ts
@@ -0,0 +1,44 @@
+class Account {
+    balance: number;
+
+    get getBalance() {
+        return this.balance;
+    }
+
+    set setBalance(amount: number) {
+        this.balance += amount;
+    }
+
+    deposite(depositAmount: number) {
+        this.setBalance = depositAmount;
+    }
+
+    constructor(defaultBalance: number = 0, protected bankName: string = "happy bank", readonly interestRate: number = 0.1) {
+        this.balance = defaultBalance;
+    }
+
+    getInterestRate() {
+        return this.interestRate;
+    }
+
+    getDefaultBalance() {
+        // return this.defaultBalance;
+    }
+}
+
+class MyAccount extends Account {
+    constructor() {
+        super();
+        this.deposite(1000);
+        this.setBalance = 1000;
+        console.log(`2번) 적금 : ${this.balance}원, ${this.getBalance}원 /
+        이율 : ${this.interestRate}, ${this.getInterestRate()}% /
+        은행명 : ${this.bankName}`);
+    }
+}
+
+let account = new Account();
+console.log(`1번) 적금 : ${account.balance}원, ${account.getBalance}원 /
+        이율 : ${account.interestRate}, ${account.getInterestRate()}%`);
+
+let myAccount = new MyAccount();
